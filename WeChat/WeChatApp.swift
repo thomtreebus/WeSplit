b277b90e3614c7c8a93e3b7b155f672dc160f31b
Thom Treebus <thomtreebus@gmail.com>
2023-06-01 13:41:15 +0100

    Initial Commit

---
+++ WeChat/WeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeChatApp.swift
+//  WeChat
+//
+//  Created by Thom Treebus on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
