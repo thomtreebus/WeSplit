03d5adfd0d9ce6c8d499c06a0ccc3f7017a3390b
Thom Treebus <thomtreebus@gmail.com>
2023-06-01 13:43:29 +0100

    create form and compute amount owed per person

--- WeChat/ContentView.swift
+++ WeChat/ContentView.swift
@@ -1,21 +1,65 @@
 //
 //  ContentView.swift
-//  WeChat
+//  WeSplit
 //
-//  Created by Thom Treebus on 01/06/2023.
+//  Created by Thom Treebus on 31/05/2023.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double { // Computed variable
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = (checkAmount / 100) * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format:
+                            .currency(code: Locale.current.identifier))
+                    .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) { // identify each element by itself with \.self
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    // reflects total to be paid per person (including tip)
+                    Text(totalPerPerson, format: .currency(code: Locale.current.identifier))
+                } header: {
+                    Text("Amount owed per person")
+                }
+            }
+            .navigationTitle("WeSplit") // attach title to form inside of navigation view
         }
-        .padding()
     }
 }
 
